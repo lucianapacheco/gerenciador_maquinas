4180fab3c5ace5db673e6858e76c54e5c83d3fd8
lucianapacheco <lucianapachecomelo@gmail.com>
2021-08-31 16:54:14 -0300

    pagina home

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_204625) do
+ActiveRecord::Schema.define(version: 2021_08_31_000813) do
+
+  create_table "installs", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_installs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
+  end
 
   create_table "maquinas", force: :cascade do |t|
     t.string "tipo"
