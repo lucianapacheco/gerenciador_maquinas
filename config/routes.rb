05c8e9910f8b479ccb26de7bc0c783820bb352e0
lucianapacheco <lucianapachecomelo@gmail.com>
2021-09-02 11:49:02 -0300

    paginas_sobre_contato

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :maquinas
   resources :home
+  resources :contato
+  resources :sobre
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
